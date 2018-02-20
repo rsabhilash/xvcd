c8520b518f5b9ddbbaee8887644a4c8c9bd6d2b4
daq <daq@d-o755-Virginia.EDU>
2018-02-20 16:23:55 -0500

    Added a make script for SLC6

---
+++ src/Make_slc6.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Use this script for building on a Raspberry Pi (even the Zero)
+# running Raspbian. Tested with Raspbian 2017-11-29, Linux Kernel
+# 4.9.59+.
+#
+# Need libusb and libftdi installed through apt-get before running
+# this shell script. An example of how to do this follows.
+#
+# sudo apt-get update
+# sudo apt-get install libusb-1.0-0
+# sudo apt-get install libftdi1-dev
+#
+# NOTE: Once have the executables built and running, may need to use
+# the "-f" option to lower the frequency that Vivado picks.
+
+echo "make USE_GETIFADDRS=eth0 USE_LIBFTDI1=true"
+make USE_GETIFADDRS=eth0 USE_LIBFTDI1=true
